973df83e70d5860fc61dd7e45160b85121c916f3
rookie <714727050@qq.com>
2019-10-06 22:20:34 +0800

    Create python_calc.py

---
+++ python_calc.py
@@ -0,0 +1,12 @@
+#-*-coding:utf-8-*-
+from tkinter import *
+#创建窗口
+window = Tk()
+#设置窗口标题
+window.title("this is a test window")
+#设置窗口大小
+window.geometry("480x400")
+#设置窗口的width不可变，height可变
+window.resizable(width=False,height=True)
+#将窗口进行无线循环
+window.mainloop()
